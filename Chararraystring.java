2913c4b167b101fa1431184ee33c433db4154d81
saba <sabasaniya90592@gmail.com>
2024-12-16 14:31:57 +0530

    java file

---
+++ Chararraystring.java
@@ -0,0 +1,13 @@
+public class Chararraystring {
+
+ public static void main(String[] args) {
+
+ char[] charArray = {'h', 'e', 'l', 'l', 'o'};
+
+ String str = new String(charArray);
+
+ System.out.println("Converted String: " + str);
+
+ }
+
+}
